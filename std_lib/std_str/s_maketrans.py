bab865dc2ae71009a46ffdadd4780b73e02d51ed
fortunezhang <fortunezhang527@gmail.com>
2015-01-20 11:54:26 +0800

    fortune-tranlate replace str

--- std_lib/std_str/s_maketrans.py
+++ std_lib/std_str/s_maketrans.py
@@ -1,5 +1,11 @@
 #codind=utf-8
 import string
 
-leet = string.maketrans('abc', '123')
-print leet
+leet = string.maketrans('abct', '1230')
+s = 'the quick brown fox jumped over the lazy dog.t'
+print s
+print s.translate(leet)
+
+s.replace('t','T')
+print s
+print s.replace('t','T')
